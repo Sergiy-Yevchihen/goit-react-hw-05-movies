9ce67980db4d3bd7160efaad85b04e702afa86df
Serhii Yevchishen <magodza06@gmail.com>
2023-06-08 19:48:43 +0300

    add app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,10 @@ import { NavLink, Route, Routes } from "react-router-dom";
 export const App = () => {
   return (
     <div>
-      <nav>
-        <NavLink>Home</NavLink>
-        <NavLink>Movies</NavLink>
-      </nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/movies">Movies</NavLink></li>
+      </ul>
       <Routes>
         <Route path="/" element={<div>Home</div>} />
         <Route path="/movies" element={<div>Movies</div>} />
