262476cf37b7ff303d32a96a317b7f2df786fc6c
Serhii Yevchishen <magodza06@gmail.com>
2023-06-08 18:55:54 +0300

    add basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
+
 import { Routes } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       
-    style={{
+    {/* style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
-      }} 
+        color: '#010101' */}
+      {/* }}  */}
     <Routes>
   React homework template
       </Routes>
