dd72a740c87168a916c05f0ac2db0d779c027d44
Serhii Yevchishen <magodza06@gmail.com>
2023-06-08 18:48:22 +0300

    add npm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
+    <div>
+      
+    style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
+      }} 
+    <Routes>
+  React homework template
+      </Routes>
     </div>
   );
 };
