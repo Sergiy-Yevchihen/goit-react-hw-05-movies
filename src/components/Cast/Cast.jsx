140f6a4370ddf61c10b2450b043c1ee8603f610e
Serhii Yevchishen <magodza06@gmail.com>
2023-06-10 16:15:14 +0300

    add https

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getMovieCredits } from './../../services/apiService';
 import { CastItem, CastList, Character, Name } from './Cast.styled';
-// import Image from '../Img/no_image.jpg';
+
 
 const Cast = () => {
   const [castList, setCastList] = useState([]);
@@ -19,8 +19,8 @@ const Cast = () => {
                 src={
                   profile_path
                     ? `https://image.tmdb.org/t/p/w200${profile_path}`
-                    : `https://www.suryalaya.org/images/no_image.jpg`
-                  // https://upload.wikimedia.org/wikipedia/commons/9/9e/Placeholder_Person.jpg
+                    : `https://upload.wikimedia.org/wikipedia/commons/9/9e/Placeholder_Person.jpg`
+                
                 }
                 alt="actor"
                 loading="lazy"
